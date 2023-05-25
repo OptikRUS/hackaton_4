060061197625f57a10ba9930649311e72b6fa863
optikrus <optikrus61@gmail.com>
2023-05-25 22:40:20 +0300

    superuser creation

---
+++ admin/users/management/commands/create_superuser.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand
+from django.db import transaction
+
+from users.models import User
+
+
+class Command(BaseCommand):
+    help = 'Create superuser django'
+
+    def add_arguments(self, parser):
+        parser.add_argument('username', type=str)
+        parser.add_argument('password', type=str)
+
+    @transaction.atomic
+    def handle(self, *args, **options):
+        self.stdout.write("Creating new superuser...")
+        username = options['username']
+        password = options['password']
+        superuser = User.objects.create_superuser(
+            username=username,
+            password=password,
+        )
+        print(f'Суперпользователь {superuser.username} создан')
